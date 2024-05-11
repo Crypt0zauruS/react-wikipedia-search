7192412246b96aea63bcd3bcd8f2c86abf7302a4
Crypt0zauruS <101745417+Crypt0zauruS@users.noreply.github.com>
2024-05-11 15:50:21 -0400

    Updated footer

--- src/App.js
+++ src/App.js
@@ -205,15 +205,6 @@ function App() {
         <h1>
           Follow me on{" "}
           <a
-            className="twitter"
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://twitter.com/CryptosaurusRe4"
-          >
-            <i className="fab fa-twitter"></i>
-          </a>{" "}
-          and{" "}
-          <a
             className="github"
             target="_blank"
             rel="noopener noreferrer"
